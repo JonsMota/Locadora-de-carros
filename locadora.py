c894ae8dea57121edb4abfb3799c5433e1d13bb2
JonsMota <jonsmota@gmail.com>
2023-11-12 15:57:10 -0300

    criado a interface

--- locadora.py
+++ locadora.py
@@ -12,12 +12,35 @@ carros = [
         ]
 alugados = []  
 
-print("--------------------") 
-print("Locadora de carros!")
-print("--------------------") 
-
 def mostrar_lista_de_carros(lista_de_carros):
     for i, car in enumerate(lista_de_carros):
         print("[{}] {} - R$ {} /dia.".format(i, car[0], car[1]))
 
-mostrar_lista_de_carros(carros)      
+mostrar_lista_de_carros(carros)        
+
+while True:
+    os.system("clear") 
+    print("--------------------") 
+    print("Locadora de carros!")
+    print("--------------------") 
+    print("O que deseja fazer?") 
+    print("0 - Mostrar portifólio | 1 - Alugar carro | 2 - Devolver carro")
+    op = int(input())
+
+    os.system("clear")
+    if op == 0:
+        pass
+
+    elif op == 1:
+        pass
+
+    elif op == 2:
+        pass
+
+    print("") 
+    print("=========")
+    print("0 - CONTINUAR | 1 - SAIR")
+    if float(input()) == 1:
+        break
+
+  
